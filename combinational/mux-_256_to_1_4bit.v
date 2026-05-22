02ed043f39c5b48a38ea2337029672dbccbe885e
Pulkit-10-0 <pulkitkapur100@gmail.com>
2026-05-22 23:21:33 +0530

    Added 256:1 4-bit multiplexer design

---
+++ combinational/mux-_256_to_1_4bit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.05.2026 12:36:42
+// Design Name: 
+// Module Name: mux-_256_to_1_4bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux-_256_to_1_4bit(
+
+    );
+endmodule
